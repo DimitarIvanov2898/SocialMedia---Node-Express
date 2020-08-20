77abf5acb6e106bda6423e6c97163c33b7d6b8bf
Dimitar <dimitarivanov2989@gmail.com>
2020-08-20 17:41:01 +0300

    Header and footer separated

--- app.js
+++ app.js
@@ -22,6 +22,11 @@ app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 app.use(flash())
 
+app.use(function(req, res, next){
+    res.locals.user = req.session.user
+    next()
+})
+
 app.set("views", "views")
 app.set("view engine", "ejs")
 
--- router.js
+++ router.js
@@ -1,10 +1,16 @@
 const express = require("express")
 const router = express.Router()
 const userController = require("./controllers/userController")
+const postController = require("./controllers/postController")
 
 router.get("/", userController.home)
+
+//user routes
 router.post("/register", userController.register)
 router.post("/login", userController.login)
 router.post("/logout", userController.logout)
+
+//post routes
+router.get("/create-post", userController.checkLogin, postController.viewCreateScreen)
 
 module.exports = router
